4274bd2f2e2974f6a88137f4ea7448600b660d92
Julien Boulen <julien.boulen@univ-rennes2.fr>
2026-09-22 17:16:29 +0200

    feat(core): implémente une méthode pour faciliter la comparaison de 2 objets

--- classes/data_controller.php
+++ classes/data_controller.php
@@ -224,4 +224,32 @@ class data_controller extends \core_customfield\data_controller {
             'end' => userdate($value['end'], $format),
         ]);
     }
+
+    /**
+     * Compare l'objet courant avec un autre objet.
+     *
+     * @param \core_customfield\data_controller $otherobject Un objet a comparé avec l'objet courant.
+     *
+     * @return int Retourne 0 si les 2 objets sont identiques,
+     *             un entier négatif si l'objet courant est inférieur à l'autre objet ou
+     *             un entier positif si l'objet courant est supérieur à l'autre objet.
+     */
+    public function compare_with(\core_customfield\data_controller $otherobject): int {
+        $value1 = json_decode($this->get_value(), $associative = true);
+        $value2 = json_decode($otherobject->get_value(), $associative = true);
+
+        if ($value1 === $value2) {
+            return 0;
+        }
+
+        if (empty($value1) === true || isset($value1['start']) === false) {
+            return -1;
+        }
+
+        if (empty($value2) === true || isset($value2['start']) === false) {
+            return 1;
+        }
+
+        return strcoll($value1['start'], $value2['start']);
+    }
 }
